466b1e30e1610e70e95ba25048b89841c386fba0
Samuel Angel Perez <sangelp@unal.edu.co>
2024-02-23 14:42:20 -0500

    Agregando metodos del servicio

--- MELI/src/main/java/com/freecupon/MELI/domain/services/exchanged/ItemsExchangedService.java
+++ MELI/src/main/java/com/freecupon/MELI/domain/services/exchanged/ItemsExchangedService.java
@@ -1,4 +1,13 @@
 package com.freecupon.MELI.domain.services.exchanged;
 
+import com.freecupon.MELI.domain.dto.ItemsExchangedDto;
+
+import java.util.Map;
+
 public interface ItemsExchangedService {
+
+    ItemsExchangedDto saveItem(ItemsExchangedDto itemsExchangedDto);
+    boolean updateItemExchanged(Map<String,Object> partialUpdate, String itemId);
+    ItemsExchangedDto findByItemId(String itemId);
+
 }
--- MELI/src/main/java/com/freecupon/MELI/repository/ItemsExchangedRepository.java
+++ MELI/src/main/java/com/freecupon/MELI/repository/ItemsExchangedRepository.java
@@ -2,7 +2,9 @@ package com.freecupon.MELI.repository;
 
 import com.freecupon.MELI.domain.models.ItemsExchanged;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemsExchangedRepository extends JpaRepository<ItemsExchanged,Long> {
 
     ItemsExchanged findByItemId(String itemId);
