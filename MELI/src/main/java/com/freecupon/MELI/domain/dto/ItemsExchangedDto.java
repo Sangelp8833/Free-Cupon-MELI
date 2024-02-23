575059a04dce4ae1cc1e388a96dea8e74f7a80e3
Samuel Angel Perez <sangelp@unal.edu.co>
2024-02-23 14:19:54 -0500

    Agregando Dto para realizar las operaciones con los Items

---
+++ MELI/src/main/java/com/freecupon/MELI/domain/dto/ItemsExchangedDto.java
@@ -0,0 +1,14 @@
+package com.freecupon.MELI.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class ItemsExchangedDto {
+
+    private String item_id;
+    private Integer exchanged;
+
+}
